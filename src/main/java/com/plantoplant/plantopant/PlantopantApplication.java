be36da9487a727ab7526d5e0cfa1d372ad2feaeb
NadsonMagno <143035086+NadsonMagno@users.noreply.github.com>
2025-03-23 17:42:55 -0300

    iniciando projeto

---
+++ src/main/java/com/plantoplant/plantopant/PlantopantApplication.java
@@ -0,0 +1,13 @@
+package com.plantoplant.plantopant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantopantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantopantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantoplant/plantopant/PlantopantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantoplant.plantopant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantopantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
